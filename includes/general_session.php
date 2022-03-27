c3ad886d91f54545c6325c36d461936dd557eefd
ephraimmode <ephraimmode@gmail.com>
2022-03-27 04:18:33 +0100

    aaa

--- includes/general_session.php
+++ includes/general_session.php
@@ -2,10 +2,10 @@
 
 //Start session: this will allow all users (logged in and non-loggedin users) to access every page it is present
 session_start();
-
+//Check if the user is loggedin before creating variable...
 if (isset($_SESSION['user'])) {
 	$session = $_SESSION['user'];
 }
 
-
 ?>
+
